71da297ba56619228ade77ec5b38e439da55fd87
Julien Deantoni <julien.deantoni@inria.fr>
2023-05-25 22:03:16 +0200

    WIP compiler toward a manually specified intermediate representation artifact  based on scxml and C/C++ code

--- sos_examples/testSoSLang/src/cli/generator_manuallycodedReference.ts
+++ sos_examples/testSoSLang/src/cli/generator_manuallycodedReference.ts
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import { AstNode, CompositeGeneratorNode, NL, Reference, isReference, streamAllContents, streamAst, toString } from 'langium';
 import path from 'path';
-import { Model, isAssignment, isBloc, isIf, isModel, isPlus, isVarDecl, isVarRef } from '../language-server/generated/ast';
+import { Model, isAssignment, isBloc, isIf, isModel, isParallelBloc, isPlus, isVarDecl, isVarRef } from '../language-server/generated/ast';
 import { extractDestinationAndName } from './cli-util';
 import { Range, integer } from 'vscode-languageclient';
 
@@ -11,12 +11,12 @@ var globalVariableCounter:integer = 0
 
 export function generateJavaScript(model: Model, filePath: string, destination: string | undefined): string {
     const data = extractDestinationAndName(filePath, destination);
-    const generatedCCSLFilePath = `${path.join(data.destination, data.name)}.lc`;
+    const generatedSCXMLFilePath = `${path.join(data.destination, data.name)}.scxml`;
 
-    const ccslFile = new CompositeGeneratorNode();
+    const scxmlFile = new CompositeGeneratorNode('');
 
-    ccslFile.append(`Specification ${data.name} {`, NL, NL);
-    generateCCSL(ccslFile, model);
+    generateSCXML(scxmlFile, model);
+
 
     const generatedCodeFilePath = `${path.join(data.destination, data.name)}.c`;
     const codeFile = new CompositeGeneratorNode();
@@ -29,11 +29,11 @@ export function generateJavaScript(model: Model, filePath: string, destination: 
     if (!fs.existsSync(data.destination)) {
         fs.mkdirSync(data.destination, { recursive: true });
     }
-    fs.writeFileSync(generatedCCSLFilePath, toString(ccslFile));
+    fs.writeFileSync(generatedSCXMLFilePath, toString(scxmlFile));
     fs.writeFileSync(generatedCodeFilePath, toString(codeFile));
     fs.writeFileSync(generatedHeaderFilePath, toString(headerFile));
 
-    return generatedCCSLFilePath;
+    return generatedSCXMLFilePath;
 }
 
 
@@ -98,148 +98,242 @@ function generateCode(codeFile: CompositeGeneratorNode, headerFile: CompositeGen
 
 
 
-function generateCCSL(ccslFile: CompositeGeneratorNode, model: Model) {
+function generateSCXML(scxmlFile: CompositeGeneratorNode, model: Model) {
     var allClocks: String[] = [];
     for (var node of streamAst(model)) {
-        if(isBloc(node) || isModel(node) || isAssignment(node) || isVarDecl(node) || isIf(node)){ //for non atomic rules
-            allClocks.push(getName(node) + "_startEvaluation");
-            allClocks.push(getName(node) + "_finishEvaluation");
-        }
-        if (isIf(node)) {
-            allClocks.push(getName(node) + "_evalCond");
-            allClocks.push(getName(node) + "_condTrue");
-            allClocks.push(getName(node) + "_condFalse");
+        if( isModel(node) || isBloc(node) || isParallelBloc(node) || isVarDecl(node) || isIf(node) || isAssignment(node) || isPlus(node)){ //for all opened Concept
+            allClocks.push(getName(node) + "_start");
+            allClocks.push(getName(node) + "_finish");
         }
     }
-    ccslFile.append(`
-    Clock ${allClocks.join(` 
-        `)} [`, NL
+     scxmlFile.append(`
+<?xml version="1.0" encoding="UTF-8"?>
+<scxml xmlns="http://www.w3.org/2005/07/scxml" version="1.0" binding="early" xmlns:qt="http://www.qt.io/2015/02/scxml-ext" qt:editorversion="10.0.1" initial="mainState">
+    <state id="mainState">
+        <parallel id="allRules">`, NL
     );
+    for (var node of streamAst(model)) {
+        if( isModel(node)){
+            const modelName: string = getName(node)
+            if (node.statements.length > 0){
+                scxmlFile.append(`
+                <state id="${modelName}" initial="${modelName}.start${getName(model.statements[0])}">
+                        <state id="${modelName}.idle">
+                            <transition type="internal" event="${modelName}.start" target="${modelName}.start${getName(model.statements[0])}"/>
+                        </state>`, NL
+                );     
 
-    ccslFile.append(`
-        Precedence ${getName(model) + '_startEvaluation'} < (max: 1) ${getName(model) + '_finishEvaluation'}`, NL
-    );
-
-    ccslFile.append(`
-            SubClocking ${getName(model) + '_startEvaluation'} <- ${getName(model.statements[0]) + '_startEvaluation'} //should be the first non atomic one
-            SubClocking ${getName(model.statements[0]) + '_startEvaluation'} <- ${getName(model) + '_startEvaluation'}`, NL
-    );
-
-    ccslFile.append(`
-            SubClocking ${getName(model) + '_finishEvaluation'} <- ${getName(model.statements[model.statements.length-1]) + '_finishEvaluation'}
-            SubClocking ${getName(model.statements[model.statements.length-1]) + '_finishEvaluation'} <- ${getName(model) + '_finishEvaluation'}`, NL
-    );
-
-    //model for loop
-    var precedesList: String[][] = [];
-    for (let i = 0; i < model.statements.length - 1; i++) {
-        const e1 = model.statements[i];
-        const e2 = model.statements[i + 1];
-        precedesList.push([getName(e1) + "_finishEvaluation", getName(e2) + "_startEvaluation"]);
-    }
-    var flatList: String[] = precedesList.map(sl => "Precedence "+sl.join(" < "));
-    console.log(flatList);
-    ccslFile.append(`
-            ${flatList.join(`
-            `)}`, NL
-    );
-
-
-    for (var node of streamAllContents(model)) {
+                for (let index = 0; index < model.statements.length; index++) {
+                    const statement: AstNode = model.statements[index];
+                    const statementName: string = getName(statement)
+                    const nexStateName: string = index == model.statements.length-1 ? modelName+".idle": modelName+".start"+getName(node.statements[index+1])
+                    scxmlFile.append(`
+                    <state id="${modelName}.start${statementName}">
+                            <onentry>
+                                <raise event="${statementName}.start"/>
+                            </onentry>
+                            <transition type="internal" event="${statementName}.finish" target="${nexStateName}"/>
+                    </state>`, NL
+                    );     
+                }
+                scxmlFile.append(`
+                </state>`, NL
+                );  
+            }
+        }
         if(isBloc(node)){
+            const blocName: string = getName(node)
+            if (node.statements.length > 0){
+                scxmlFile.append(`
+                <state id="${blocName}" initial="${blocName}.start${getName(model.statements[0])}">
+                        <state id="${blocName}.idle">
+                            <transition type="internal" event="${blocName}.start" target="${blocName}.start${getName(node.statements[0])}"/>
+                        </state>`, NL
+                );     
 
-            ccslFile.append(` //bloc stuff
-                    Precedence ${getName(node) + '_startEvaluation'} < (max: 1) ${getName(node) + '_finishEvaluation'}`, NL
-           );
-
-            ccslFile.append(`
-                    SubClocking ${getName(node) + '_startEvaluation'} <- ${getName(node.statements[0]) + '_startEvaluation'} //should be the first non atomic one
-                    SubClocking ${getName(node.statements[0]) + '_startEvaluation'} <- ${getName(node) + '_startEvaluation'}`, NL
-            );
-
-            ccslFile.append(`
-                    SubClocking ${getName(node) + '_finishEvaluation'} <- ${getName(node.statements[node.statements.length-1]) + '_finishEvaluation'}
-                    SubClocking ${getName(node.statements[node.statements.length-1]) + '_finishEvaluation'} <- ${getName(node) + '_finishEvaluation'}`, NL
-            );
-
-
-            var precedesList: String[][] = [];
-            for (let i = 0; i < node.statements.length - 1; i++) {
-                const e1 = node.statements[i];
-                const e2 = node.statements[i + 1];
-                precedesList.push([getName(e1) + "_finishEvaluation", getName(e2) + "_startEvaluation"]);
+                for (let index = 0; index < model.statements.length; index++) {
+                    const statement: AstNode = model.statements[index];
+                    const statementName: string = getName(statement)
+                    const nexStateName: string = index == model.statements.length-1 ? blocName+".idle": blocName+".start"+getName(node.statements[index+1])
+                    scxmlFile.append(`
+                    <state id="${blocName}.start${statementName}">
+                            <onentry>
+                                <raise event="${statementName}.start"/>
+                            </onentry>
+                            <transition type="internal" event="${statementName}.finish" target="${nexStateName}"/>
+                    </state>`, NL
+                    );     
+                }
+                scxmlFile.append(`
+                </state>`, NL
+                );  
             }
-            var flatList: String[] = precedesList.map(sl => "Precedence "+sl.join(" < "));
-            console.log(flatList);
-            ccslFile.append(`
-                    ${flatList.join(`
-                    `)}`, NL
-            );
         }
+        if(isParallelBloc(node)){
+            const blocName: string = getName(node)
+            if (node.statements.length > 0){
+                scxmlFile.append(`
+                <state id="${blocName}" initial="${blocName}c.idle">
+                    <state id="${blocName}.idle">
+                        <transition type="internal" event="${blocName}.start" target="${blocName}.startAllStatements">
+                            <log label="start ${blocName}"/>
+                        </transition>
+                    </state>
+                    <parallel id="${blocName}.startAllStatements">`, NL
+                );     
 
-        if (isVarDecl(node)) { //VarDecl is not atomic
-            ccslFile.append(`//varDecl stuff
-            Precedence ${getName(node) + '_startEvaluation'} < ${getName(node) + '_finishEvaluation'}`,NL
-            );
-        }
-        
-        if (isAssignment(node)) { //Assignment is not atomic
-            ccslFile.append(`//assignement stuff
-            Precedence ${getName(node) + '_startEvaluation'} < ${getName(node) + '_finishEvaluation'}`, NL
-            );
-
-        }
-        
-        if (isIf(node)) { //IF is not atomic
-            ccslFile.append(`//if stuff
-            Precedence ${getName(node)}_startEvaluation < ${getName(node)}_finishEvaluation`, NL
-            );
-            //other sched rules
-            ccslFile.append(`
-            SubClocking ${getName(node) + '_startEvaluation'} <- ${getName(node) + '_evalCond'}
-            SubClocking ${getName(node) + '_evalCond'} <- ${getName(node) + '_startEvaluation'}`, NL
-            );
-
-            ccslFile.append(`
-            Let tmp_${getName(node)}_xor be ${getName(node)}_condTrue + ${getName(node)}_condFalse //may be 'or' instead of +
-            Exclusion ${getName(node)}_condTrue # ${getName(node)}_condFalse
-            Precedence ${getName(node)}_evalCond < tmp_${getName(node)}_xor`, NL
-            );
-
-            ccslFile.append(`
-            SubClocking ${getName(node) + '_condTrue'} <- ${getName(node.then) + '_startEvaluation'}
-            SubClocking ${getName(node.then) + '_startEvaluation'} <- ${getName(node) + '_condTrue'}`, NL
-            );
-
-            if(node.else){
-                ccslFile.append(`
-                SubClocking ${getName(node) + '_condFalse'} <- ${getName(node.else) + '_startEvaluation'}
-                SubClocking ${getName(node.else) + '_startEvaluation'} <- ${getName(node) + '_condFalse'}`, NL
-                );
-
-                ccslFile.append(`
-                Let tmp_${getName(node)}_endThenOrElse be ${getName(node.then)}_finishEvaluation + ${getName(node.else)}_finishEvaluation //may be 'or' instead of +
-                Precedence tmp_${getName(node)}_endThenOrElse < ${getName(node)}_finishEvaluation`, NL
-                );
-            }else{
-                ccslFile.append(`
-                Precedence ${getName(node.then)}_finishEvaluation < ${getName(node)}_finishEvaluation`, NL
-                );
+                for (let index = 0; index < model.statements.length; index++) {
+                    const statement: AstNode = model.statements[index];
+                    const statementName: string = getName(statement)
+                    const nexStateName: string = blocName+".finish"+statementName
+                    scxmlFile.append(`
+                    <state id="${blocName}.v1Assign1" initial="${blocName}.startv1Assign1">
+                        <state id="${blocName}.start${statementName}">
+                                <onentry>
+                                    <raise event="${statementName}.start"/>
+                                </onentry>
+                                <transition type="internal" event="${statementName}.finish" target="${nexStateName}"/>
+                        </state>
+                        <final id="${nexStateName}">
+                        </final>`, NL
+                    );     
+                }
+                scxmlFile.append(`
+                    <transition type="internal" event="done.state.${blocName}.startAllStatements" target="${blocName}.idle">
+                        <log label="finish ${blocName}"/>
+                        <raise event="${blocName}.finish"/>
+                    </transition>
+                    </parallel>
+                </state>`, NL
+                );  
             }
+        }
+
+    }
+
+
+
+    // scxmlFile.append(`
+    //         SubClocking ${modelName + '_startEvaluation'} <- ${getName(model.statements[0]) + '_startEvaluation'} //should be the first non atomic one
+    //         SubClocking ${getName(model.statements[0]) + '_startEvaluation'} <- ${modelName + '_startEvaluation'}`, NL
+    // );
+
+    // scxmlFile.append(`
+    //         SubClocking ${modelName + '_finishEvaluation'} <- ${getName(model.statements[model.statements.length-1]) + '_finishEvaluation'}
+    //         SubClocking ${getName(model.statements[model.statements.length-1]) + '_finishEvaluation'} <- ${modelName + '_finishEvaluation'}`, NL
+    // );
+
+    // //model for loop
+    // var precedesList: String[][] = [];
+    // for (let i = 0; i < model.statements.length - 1; i++) {
+    //     const e1 = model.statements[i];
+    //     const e2 = model.statements[i + 1];
+    //     precedesList.push([getName(e1) + "_finishEvaluation", getName(e2) + "_startEvaluation"]);
+    // }
+    // var flatList: String[] = precedesList.map(sl => "Precedence "+sl.join(" < "));
+    // console.log(flatList);
+    // scxmlFile.append(`
+    //         ${flatList.join(`
+    //         `)}`, NL
+    // );
+
+
+    // for (var node of streamAllContents(model)) {
+    //     if(isBloc(node)){
+
+    //         scxmlFile.append(` //bloc stuff
+    //                 Precedence ${getName(node) + '_startEvaluation'} < (max: 1) ${getName(node) + '_finishEvaluation'}`, NL
+    //        );
+
+    //         scxmlFile.append(`
+    //                 SubClocking ${getName(node) + '_startEvaluation'} <- ${getName(node.statements[0]) + '_startEvaluation'} //should be the first non atomic one
+    //                 SubClocking ${getName(node.statements[0]) + '_startEvaluation'} <- ${getName(node) + '_startEvaluation'}`, NL
+    //         );
+
+    //         scxmlFile.append(`
+    //                 SubClocking ${getName(node) + '_finishEvaluation'} <- ${getName(node.statements[node.statements.length-1]) + '_finishEvaluation'}
+    //                 SubClocking ${getName(node.statements[node.statements.length-1]) + '_finishEvaluation'} <- ${getName(node) + '_finishEvaluation'}`, NL
+    //         );
+
+
+    //         var precedesList: String[][] = [];
+    //         for (let i = 0; i < node.statements.length - 1; i++) {
+    //             const e1 = node.statements[i];
+    //             const e2 = node.statements[i + 1];
+    //             precedesList.push([getName(e1) + "_finishEvaluation", getName(e2) + "_startEvaluation"]);
+    //         }
+    //         var flatList: String[] = precedesList.map(sl => "Precedence "+sl.join(" < "));
+    //         console.log(flatList);
+    //         scxmlFile.append(`
+    //                 ${flatList.join(`
+    //                 `)}`, NL
+    //         );
+    //     }
+
+    //     if (isVarDecl(node)) { //VarDecl is not atomic
+    //         scxmlFile.append(`//varDecl stuff
+    //         Precedence ${getName(node) + '_startEvaluation'} < ${getName(node) + '_finishEvaluation'}`,NL
+    //         );
+    //     }
+        
+    //     if (isAssignment(node)) { //Assignment is not atomic
+    //         scxmlFile.append(`//assignement stuff
+    //         Precedence ${getName(node) + '_startEvaluation'} < ${getName(node) + '_finishEvaluation'}`, NL
+    //         );
+
+    //     }
+        
+    //     if (isIf(node)) { //IF is not atomic
+    //         scxmlFile.append(`//if stuff
+    //         Precedence ${getName(node)}_startEvaluation < ${getName(node)}_finishEvaluation`, NL
+    //         );
+    //         //other sched rules
+    //         scxmlFile.append(`
+    //         SubClocking ${getName(node) + '_startEvaluation'} <- ${getName(node) + '_evalCond'}
+    //         SubClocking ${getName(node) + '_evalCond'} <- ${getName(node) + '_startEvaluation'}`, NL
+    //         );
+
+    //         scxmlFile.append(`
+    //         Let tmp_${getName(node)}_xor be ${getName(node)}_condTrue + ${getName(node)}_condFalse //may be 'or' instead of +
+    //         Exclusion ${getName(node)}_condTrue # ${getName(node)}_condFalse
+    //         Precedence ${getName(node)}_evalCond < tmp_${getName(node)}_xor`, NL
+    //         );
+
+    //         scxmlFile.append(`
+    //         SubClocking ${getName(node) + '_condTrue'} <- ${getName(node.then) + '_startEvaluation'}
+    //         SubClocking ${getName(node.then) + '_startEvaluation'} <- ${getName(node) + '_condTrue'}`, NL
+    //         );
+
+    //         if(node.else){
+    //             scxmlFile.append(`
+    //             SubClocking ${getName(node) + '_condFalse'} <- ${getName(node.else) + '_startEvaluation'}
+    //             SubClocking ${getName(node.else) + '_startEvaluation'} <- ${getName(node) + '_condFalse'}`, NL
+    //             );
+
+    //             scxmlFile.append(`
+    //             Let tmp_${getName(node)}_endThenOrElse be ${getName(node.then)}_finishEvaluation + ${getName(node.else)}_finishEvaluation //may be 'or' instead of +
+    //             Precedence tmp_${getName(node)}_endThenOrElse < ${getName(node)}_finishEvaluation`, NL
+    //             );
+    //         }else{
+    //             scxmlFile.append(`
+    //             Precedence ${getName(node.then)}_finishEvaluation < ${getName(node)}_finishEvaluation`, NL
+    //             );
+    //         }
 
             
 
-        }
+    //     }
 
-    }
+    // }
 
-    ccslFile.append(`
-    ]
-    //Where should we put the data dependent control specification ?`, NL
+    scxmlFile.append(`
+        </parallel>
+    </state>
+</scxml>`, NL
     );
 
 
-    ccslFile.append(`
+    scxmlFile.append(`
     }
     `, NL);
 }
