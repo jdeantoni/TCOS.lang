699a98f12cd0c0519b3a80b16cab68faad852363
Swannben <swannbenz@gmail.com>
2024-07-24 13:39:05 +0200

    reimplementation of changes that were lost

--- examples/languages/testSoSLang/src/cli/index.ts
+++ examples/languages/testSoSLang/src/cli/index.ts
@@ -4,19 +4,30 @@ import { Model } from '../language-server/generated/ast';
 import { SimpleLLanguageMetaData } from '../language-server/generated/module';
 import { createSimpleLServices } from '../language-server/simple-l-module';
 import { extractAstNode } from './cli-util';
-import { generateCPPfromCCFG } from './generatorCPPfromCCFG';
 import { NodeFileSystem } from 'langium/node';
+import { generatefromCCFG } from './interpretCCFG';
+import { IGenerator } from './GeneratorInterface';
+import { PythonGenerator } from './pythonGenerator';
+import { CppGenerator } from './cppGenerator';
 
 export const generateAction = async (fileName: string, opts: GenerateOptions): Promise<void> => {
     const services = createSimpleLServices(NodeFileSystem).SimpleL;
     const model = await extractAstNode<Model>(fileName, services);
-    const generatedFilePath = generateCPPfromCCFG(model, fileName, opts.targetDirectory, opts.debug);
-    console.log(chalk.green(`CCFG and C++ Code generated successfully: ${generatedFilePath}`));
+    let generatedFilePath;
+    let generator:IGenerator;
+    if (opts.python) {
+        generator = new PythonGenerator();
+    } else {
+        generator = new CppGenerator();
+    }
+    generatedFilePath = generatefromCCFG(model, fileName, opts.targetDirectory, opts.debug,generator);
+    console.log(chalk.green(`CCFG and Code generated successfully: ${generatedFilePath}`));
 };
 
 export type GenerateOptions = {
     targetDirectory ?: string;
     debug ?: boolean;
+    python ?: boolean;
 }
 
 export default function(): void {
@@ -32,6 +43,7 @@ export default function(): void {
         .argument('<file>', `source file (possible file extensions: ${fileExtensions})`)
         .option('-t, --targetDirectory <dir>', 'destination directory of generating', 'generated')
         .option('-d, --debug ', 'ask for debugging message during execution of the generated code')
+        .option('--python', 'compile into python', 'output.cpp')
         .description('generates the concurrent control flow graph representation of the given source file')
         .action(generateAction);
 
