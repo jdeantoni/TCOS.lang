61e40f3a35a3ef106146ab83252152eacf026801
Bence-dp <bencediplacido16@gmail.com>
2026-07-10 11:35:11 +0200

    support multiple simultaneous language extensions for debug

--- packages/TCOS/src/cli/facade/debugConfigGeneration.ts
+++ packages/TCOS/src/cli/facade/debugConfigGeneration.ts
@@ -5,6 +5,7 @@ import { CompositeGeneratorNode, NL, toString } from "langium/generate";
 import type { SoSSpec } from "../../language-server/generated/ast.js";
 import { extractDestinationAndName } from "../cli-util.js";
 
+
 export function generateDebugConfigFromSoS(
     model: SoSSpec,
     filePath: string,
@@ -39,37 +40,44 @@ function appendDebugConfigurationProvider(
 
     file.append(`
 import * as vscode from 'vscode';
+import fs from "node:fs";
+import { fileURLToPath, pathToFileURL } from "node:url";
+import path from "node:path";
 
 export class ${model.name}DebugConfigurationProvider
 implements vscode.DebugConfigurationProvider {
 
     constructor(private context: vscode.ExtensionContext) {}
-
+    
     resolveDebugConfiguration(
         folder: vscode.WorkspaceFolder | undefined,
         config: vscode.DebugConfiguration
     ): vscode.ProviderResult<vscode.DebugConfiguration> {
 
         const editor = vscode.window.activeTextEditor;
-
+        
         if (!editor) {
             vscode.window.showErrorMessage("No active editor.");
             return undefined;
         }
+        const packageJson = JSON.parse(fs.readFileSync(path.resolve(__dirname, "../../package.json"), "utf8"));
+        const languageIdent = packageJson.contributes.languages[0].id;
 
-        config.type = "ccfg";
-        config.request = "launch";
+        if (editor.document.languageId === languageIdent){
+            config.type = "ccfg";
+            config.request = "launch";
 
-        config.name = config.name ?? "Debug ${model.name}";
+            config.name = config.name ?? "Debug ${model.name}";
 
-        config.sourceFile = editor.document.fileName;
-        config.debugServer = 4711;
-        config.language = "${model.name}";
+            config.sourceFile = editor.document.fileName;
+            config.debugServer = 4711;
+            config.language = "${model.name}";
 
-        config.facadePath =
-            this.context.asAbsolutePath(
-                "out/cli/generated/${compilerName}DapFacade.js"
-            );
+            config.facadePath =
+                this.context.asAbsolutePath(
+                    "out/cli/generated/${compilerName}DapFacade.js"
+                );
+        }
 
         return config;
     }
